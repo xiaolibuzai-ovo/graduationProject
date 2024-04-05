0837485fa74493fe1dd166105f5206dc6cb1baba
limingzhi <3163678221@qq.com>
2024-04-05 14:23:54 +0800

    feat: init repo

--- server/main.go
+++ server/main.go
@@ -1,18 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	// 1. 创建路由
 	r := gin.Default()
 
-	// 2. 绑定路由规则，执行的函数
-	// gin.Context，封装了request和response
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello world!")
+	r.GET("ping", func(c *gin.Context) {
+		c.Status(http.StatusOK)
 	})
 
-	// 3. 监听端口，默认在8080
-	// Run("里面不指定端口号默认为8080")
 	r.Run(":8000")
 }
